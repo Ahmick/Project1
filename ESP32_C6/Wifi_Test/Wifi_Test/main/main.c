70fb94591b4a3573f42c2fc9b30a9e1483e9c202
Ahmick <cdk57@pm.me>
2025-04-09 10:56:25 -0400

    Example_projects, BME680 and reorg
    Starting to work on I2C BME680 project. Added data sheet and BME680 api.
    
    Added i2c example projects
    
    Reorgaized test project to wifi_test.
    
    Wifi_test is connecting to internet
    
    BME680 is new project folder.

---
+++ ESP32_C6/Wifi_Test/Wifi_Test/main/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#define WIFI_SSID      "COGECO-9E02B8"
+#define WIFI_PASSWORD  "ktyut2m2"
+#define TAG "ESP32-C6: "
+
+void initialize(void)
+{
+    printf("Initializing memory, network and creating event loop. \n");
+    nvs_flash_init();
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+        },
+    };
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    printf("Initializing done.");
+}
+
+void app_main(void)
+{
+    initialize();
+    
+    esp_wifi_start();
+    esp_wifi_connect();
+    printf("Wifi connected \n");
+    printf("That is it for now. \n");
+        
+    
+}
+
