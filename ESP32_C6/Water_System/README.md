2148f52211a43253a7f041a12670e79043930eb0
Ahmick <cdk57@pm.me>
2025-07-24 16:55:43 -0400

    Water System
    First commit for water system

--- ESP32_C6/Water_System/README.md
+++ ESP32_C6/Water_System/README.md
@@ -1,32 +1,29 @@
-# _Sample project_
+# _README_
 
-(See the README.md file in the upper level 'examples' directory for more information about examples.)
+Project Overview
 
-This is the simplest buildable example. The example is used by command `idf.py create-project`
-that copies the project to user specified path and set it's name. For more information follow the [docs page](https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/build-system.html#start-a-new-project)
+Set up ESP 32 to connect to wifi. Subscribe to a channel on MQTT. Control two relays that control two valves for in ground irrigation system.
 
+- [X] Connect to wifi
+- [X] Subscribe to a MQTT channel
+- [] Control two relays
 
+---
 
-## How to use example
-We encourage the users to use the example as a template for the new projects.
-A recommended way is to follow the instructions on a [docs page](https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/build-system.html#start-a-new-project).
+Project Details
 
-## Example folder contents
+Using my house wifi, homeassisstant rpi, esp 32, and relay module (+irrigation stuff) to control the relays automatically.
+This is to setup an inground irrigation system. 
+The idea is to use homeassistant automations to send out MQTT messages. The ESP32 will be listening. When it recieves a message it will output high on a gpio, causing a relay to turn on, powering the irrigation valve.
 
-The project **sample_project** contains one source file in C language [main.c](main/main.c). The file is located in folder [main](main).
+Subsribe to each topic (relay 1 and relay 2)
+topic is for relay to turn on
+message is for length of time.
 
-ESP-IDF projects are built using CMake. The project build configuration is contained in `CMakeLists.txt`
-files that provide set of directives and instructions describing the project's source files and targets
-(executable, library, or both). 
+I want the system to fail safe. AKA if the ESP-32 resets to default to relay off. 
 
-Below is short explanation of remaining files in the project folder.
+If the broker fails while the system is on then it won't turn off. Implement reset.
 
-```
-├── CMakeLists.txt
-├── main
-│   ├── CMakeLists.txt
-│   └── main.c
-└── README.md                  This is the file you are currently reading
-```
-Additionally, the sample project contains Makefile and component.mk files, used for the legacy Make based build system. 
-They are not used or needed when building with CMake and idf.py.
+- [] Add reset to esp. 15 seconds then turn off unless on signal.
+
+- [] Setup homeassisstant automations to send every 10 seconds while system is on.
--- ESP32_C6/Water_System/main/main.c
+++ ESP32_C6/Water_System/main/main.c
@@ -1,121 +1,124 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
 #include <string.h>
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "protocol_examples_common.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
-
-#include "lwip/sockets.h"
-#include "lwip/dns.h"
-#include "lwip/netdb.h"
-
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 #include "mqtt_client.h"
+#include "driver/gpio.h"
 
-static const char *TAG = "MQTT_EXAMPLE";
-#define  EXAMPLE_ESP_WIFI_SSID "COGECO-9E02B8"
-#define  EXAMPLE_ESP_WIFI_PASS "ktyut2m2"
- 
-uint32_t MQTT_CONNECTED = 0;
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
-static void mqtt_app_start(void);
+#define WIFI_SSID   "COGECO-9E02B8"
+#define WIFI_PASS   "ktyut2m2"
+#define GPIO_1 2
+#define GPIO_2 4
 
-void event(void *ctx, esp_event_base_t *event)
-{
+int i = 0;
+int k = 0;
+int z = 1;
+static const char *TAG = "MQTT_SUB";
+static const char *TAG1 = "wifi station";
+static const char *TAG2 = "DELAY_TASK";
 
-}
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data) {
-    if (event_base == WIFI_EVENT) {
-        switch (event_id) {
-            case WIFI_EVENT_STA_START:
-                esp_wifi_connect();
-                ESP_LOGI(TAG, "Trying to connect with Wi-Fi");
-                break;
-
-            case WIFI_EVENT_STA_CONNECTED:
-                ESP_LOGI(TAG, "Wi-Fi connected");
-                break;
-
-            case WIFI_EVENT_STA_DISCONNECTED:
-                ESP_LOGI(TAG, "Disconnected: Retrying Wi-Fi");
-                esp_wifi_connect();
-                break;
-
-            default:
-                break;
-        }
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ESP_LOGI(TAG, "Got IP: starting MQTT Client");
-        mqtt_app_start();
-    }
-}
-
-
-void wifi_init(void)
-{
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-        },
+//setup GPIO 1-2
+void setup_gpio_inputs() {
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << GPIO_1) |
+                        (1ULL << GPIO_2),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start());
+
+    gpio_config(&io_conf);
 }
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
+
+
+// Define the task function
+void task(void *pvParameters)
+{
+    TickType_t start_tick = 0; // xTaskGetTickCount(); //Gets the current tick and keeps it. Used to get elapsed time
+    TickType_t current_tick = 0;
+    //i = 0;
+    // Gets the current tick, prints the elapsed time and delays 1 second.
+
+    while (true){
+        current_tick = xTaskGetTickCount();
+        z = (current_tick - start_tick) * portTICK_PERIOD_MS 
+
+        if (i == 1 && z > 0) {
+            gpio_set_level(GPIO_1, 1);
+            ESP_LOGI(TAG2, "GPIO1 high");
+            start_tick = current_tick;
+            current_tick = xTaskGetTickCount();
+            //vTaskDelay(1000 / portTICK_PERIOD_MS);
+            
+        } else if (i == 2 && z > 0) {
+            gpio_set_level(GPIO_2, 1);
+            ESP_LOGI(TAG2, "GPIO2 high");
+            start_tick = current_tick; 
+            current_tick = xTaskGetTickCount();
+            //vTaskDelay(1000 / portTICK_PERIOD_MS);
+        } else {
+            
+            gpio_set_level(GPIO_1, 0);
+            gpio_set_level(GPIO_2, 0);
+            current_tick = xTaskGetTickCount();
+            start_tick = current_tick; 
+            ESP_LOGI(TAG2, "GPIO1 and 2 low");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            z = 1;
+        }
+        }
+        
+        current_tick = xTaskGetTickCount(); 
+        uint32_t elapsed_ms = (current_tick - start_tick) * portTICK_PERIOD_MS;
+        ESP_LOGI(TAG2, "Elapsed Time: %d ms", elapsed_ms);
+        if (elapsed_ms > i*60000 && k == 1){
+            i = 0;
+            ESP_LOGI(TAG2, "Timer reset to 0 and i set to 0");
+            start_tick = 0; // xTaskGetTickCount(); //Gets the current tick and keeps it. Used to get elapsed time
+            k = 0;
+        }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay 1 second
+
+    }
+
+    
+}
+
+//Sets up MQTT server
+
+esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = "mqtt://homeassistant.local",
+        .credentials.username = "mosquitto",
+        .credentials.authentication.password= "Mosquitto",
+    };
+
+//Set up the handler. 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
-    switch ((esp_mqtt_event_id_t)event_id)
-    {
+    switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
+        int msg_id = esp_mqtt_client_subscribe(client, "1", 0);
+        int msg_id = esp_mqtt_client_subscribe(client, "2", 0);
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        MQTT_CONNECTED=1;
-        
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/test1", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/test2", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        MQTT_CONNECTED=0;
         break;
-
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
         break;
@@ -124,14 +127,30 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         break;
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        printf("Published/n");
         break;
+    
+    //Prints event data. If data  "1 or 2" sets i to 1 or 2. 
+    
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
+        
+        if (strncmp(event->topic, "1", event->topic_len) == 0)
+        {
+            i = 1; //Relay 1 turn on 
+            //k = event->data; // Message sent tag
+        }
+        if (strncmp(event->topic, "2", event->topic_len) == 0)
+        {
+            i = 2; //Relay 2 identifier
+            //k = event->data; //Message sent tag
+        }
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        printf("Shits fucked");
         break;
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
@@ -139,41 +158,143 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
-esp_mqtt_client_handle_t client = NULL;
-static void mqtt_app_start(void)
+
+//Everything below is to setup WiFi
+
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+
+
+static int s_retry_num = 0;
+
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
-    ESP_LOGI(TAG, "STARTING MQTT");
-    esp_mqtt_client_config_t mqttConfig = {
-        .broker.address.uri = "mqtt://homeassistant.local",
-        .credentials.username = "mosquitto",
-        .credentials.authentication.password= "Mosquitto",
-    };
-    
-    client = esp_mqtt_client_init(&mqttConfig);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
-    esp_mqtt_client_start(client);
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 5) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG1, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG1,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG1, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
 }
 
-void Publisher_Task(void *params)
+void wifi_init_sta(void)
 {
-  while (true)
-  {
-    if(MQTT_CONNECTED)
-    {
-        esp_mqtt_client_publish(client, "/topic/test3", "Helllo World", 0, 0, 0);
-        vTaskDelay(15000 / portTICK_PERIOD_MS);
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
-  }
 }
 
 void app_main(void)
 {
+    //Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-    wifi_init();
-    xTaskCreate(Publisher_Task, "Publisher_Task", 1024 * 5, NULL, 5, NULL);
+
+    if (CONFIG_LOG_MAXIMUM_LEVEL > CONFIG_LOG_DEFAULT_LEVEL) {
+        /* If you only want to open more logs in the wifi module, you need to make the max level greater than the default level,
+         * and call esp_log_level_set() before esp_wifi_init() to improve the log level of the wifi module. */
+        esp_log_level_set("wifi", CONFIG_LOG_MAXIMUM_LEVEL);
+    }
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+    printf("Connected...\n");
+    
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    printf("Debug1\n");
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    printf("Debug2\n");
+    printf("%d\n",i);
+    esp_mqtt_client_start(client);
+    printf("Debug3\n");
+
+    setup_gpio_inputs();
+    // Create the FreeRTOS task
+    xTaskCreate(
+        task,              // Task function
+        "task",            // Task name
+        2048,              // Stack size in bytes
+        NULL,              // Parameters to pass
+        5,                 // Task priority
+        NULL               // Task handle
+    );
+    printf("Debug4\n");
+    printf("%d\n",i);
 }
